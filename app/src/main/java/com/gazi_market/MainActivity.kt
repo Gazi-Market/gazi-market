25daa5a52a93d10ca3c3029bd4bf1fd31c5b9975
MinseoKangQ <kms02171@naver.com>
2023-10-06 17:46:03 +0900

    Add Bottom Navigation

--- app/src/main/java/com/gazi_market/MainActivity.kt
+++ app/src/main/java/com/gazi_market/MainActivity.kt
@@ -2,10 +2,40 @@ package com.gazi_market
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.gazi_market.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(Home())
+                R.id.chat -> replaceFragment(Chat())
+                R.id.my_page -> replaceFragment(MyPage())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
