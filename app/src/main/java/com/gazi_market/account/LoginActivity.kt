b08c8db79df9d73c07fa72a6bb66188dbcae369e
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2023-11-04 22:53:56 +0900

    회원가입 및 로그인 기능 추가 (#3)
    * firebase 연동
    
    * 회원가입 및 로그인 추가

---
+++ app/src/main/java/com/gazi_market/account/LoginActivity.kt
@@ -0,0 +1,72 @@
+package com.gazi_market.account
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.gazi_market.MainActivity
+import com.gazi_market.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+class LoginActivity : AppCompatActivity() {
+    private var auth : FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth = Firebase.auth
+
+        val id = findViewById<EditText>(R.id.signupID)
+        val pwd = findViewById<EditText>(R.id.signupPassword)
+
+        // 로그인 버튼
+        val loginButton = findViewById<Button>(R.id.signup)
+        loginButton.setOnClickListener {
+            signIn(id.text.toString(),pwd.text.toString())
+        }
+    }
+
+    // 로그아웃하지 않을 시 자동 로그인 , 회원가입시 바로 로그인 됨
+    public override fun onStart() {
+        super.onStart()
+        moveMainPage(auth?.currentUser)
+    }
+
+    // 로그인
+    private fun signIn(email: String, password: String) {
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth?.signInWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext, "로그인에 성공 하였습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        moveMainPage(auth?.currentUser)
+                    } else {
+                        Toast.makeText(
+                            baseContext, "로그인에 실패 하였습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+
+
+    // 유저정보 넘겨주고 메인 액티비티 호출
+    fun moveMainPage(user: FirebaseUser?){
+        if( user!= null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+    }
+}
